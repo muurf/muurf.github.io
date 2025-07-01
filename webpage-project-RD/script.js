5e9810e34d37662734c2c1431e4d4c2b75566876
justin! <justinnguyen401@gmail.com>
2025-07-01 19:59:54 -0400

    project upload
    uploaded project website

---
+++ webpage-project-RD/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+// this is the js for the hamburger menu easy!
